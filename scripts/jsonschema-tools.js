wikimedia/jsonschema-tools

commit 706c5cd9255fd848dd503ccb81b6893d56cff70b

Add option to enforce bounds for number fields
This option configures a pair of numeric bounds that, at the
schema materialization level, will apply those bounds to number
fields (float, integer or number) by traversing the schema.

The change includes a small refactor that separates the logic that
modifies the materialized schema from the main function that
outputs the materialized schema to a path.

Bug: T258659

scripts/jsonschema-tools.js
@@ -6,8 +6,8 @@ const path  = require('path');
 const yargs = require('yargs');
 
 const {
-    dereferenceSchema,
-    materializeSchemaVersion,
+    materializeSchema,
+    materializeSchemaToPath,
     materializeModifiedSchemas,
     materializeAllSchemas,
     readObject,
@@ -181,7 +181,7 @@ async function dereference(args) {
 
     const schemas = await Promise.all(schemaPaths.map(async (schemaPath) => {
         try {
-            return await dereferenceSchema(await readObject(schemaPath), options);
+            return await materializeSchema(await readObject(schemaPath), options);
         } catch (err) {
             options.log.fatal(err, `Failed dereferencing schema at ${schemaPath}`);
             process.exit(1);
@@ -221,7 +221,7 @@ async function materialize(args) {
         try {
             options.log.info(`Reading schema from ${schemaPath}`);
             let schema = await readObject(schemaPath);
-            await materializeSchemaVersion(schemaDirectory, schema, options);
+            await materializeSchemaToPath(schemaDirectory, schema, options);
         } catch (err) {
             options.log.fatal(err, `Failed materializing schema from ${schemaPath} into ${schemaDirectory}.`);
             process.exit(1);

lib/jsonschema-tools.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const _           = require('lodash');
-const yaml        = require('js-yaml');
-const path        = require('path');
-const semver      = require('semver');
-const readdirSync = require('recursive-readdir-sync');
-const fse         = require('fs-extra');
-const pino        = require('pino');
-const util        = require('util');
-const exec        = util.promisify(require('child_process').exec);
-const RefParser   = require('json-schema-ref-parser');
-const mergeAllOf  = require('json-schema-merge-allof');
-const Promise     = require('bluebird');
+const _              = require('lodash');
+const yaml           = require('js-yaml');
+const path           = require('path');
+const semver         = require('semver');
+const readdirSync    = require('recursive-readdir-sync');
+const fse            = require('fs-extra');
+const pino           = require('pino');
+const util           = require('util');
+const exec           = util.promisify(require('child_process').exec);
+const RefParser      = require('json-schema-ref-parser');
+const mergeAllOf     = require('json-schema-merge-allof');
+const traverseSchema = require('json-schema-traverse');
+const Promise        = require('bluebird');
 
 /**
  * Default options for various functions in this library.
@@ -93,6 +94,14 @@ const defaultOptions = {
      * The keys in these config files are the same as these defaultOptions keys.
      */
     configPaths: ['./.jsonschema-tools.yaml'],
+    /**
+     * Check the existing numeric bounds for a number field, and enforce a number type.
+     * For more flexibility and clarity, number types are expressed here as an array of
+     * the minimum and maximum numbers allowed.
+     * The tool will add `minimum` and `maximum` properties if they aren't present, and
+     * will modify their value if they outside the configured bounds.
+     */
+    enforcedNumericBounds: [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER],
 };
 
 
@@ -534,6 +543,23 @@ async function dereferenceSchema(schema, options = {}) {
 }
 
 /**
+ * Outputs a given schema with any modifications specified in the options,
+ * including but not limited to dereferencing and enforcing numeric bounds.
+ *
+ * @param {Object} schema Schema to materialize
+ * @param {Object} options
+ */
+async function materializeSchema (schema, options) {
+    if (options.shouldDereference) {
+        schema = await dereferenceSchema(schema, options);
+    }
+    if (options.enforcedNumericBounds) {
+        schema = enforceNumericBounds(schema, options.enforcedNumericBounds);
+    }
+    return schema;
+}
+
+/**
  * Materializes a versioned schema file in the directory.
  *
  * @param {string} schemaDirectory directory in which to materialize schema
@@ -541,15 +567,13 @@ async function dereferenceSchema(schema, options = {}) {
  * @param {Object} options
  * @return {Promise<string>} path of newly materialized files
  */
-async function materializeSchemaVersion(schemaDirectory, schema, options = {}) {
+async function materializeSchemaToPath(schemaDirectory, schema, options = {}) {
     options = readConfig(options);
     const log = options.log;
 
     const version = schemaVersion(schema, options.schemaVersionField);
 
-    if (options.shouldDereference) {
-        schema = await dereferenceSchema(schema, options);
-    }
+    schema = await materializeSchema(schema, options);
 
     return _.flatten(await Promise.all(options.contentTypes.map(async (contentType) => {
         let materializedFiles = [];
@@ -641,7 +665,19 @@ async function materializeSchemaVersion(schemaDirectory, schema, options = {}) {
 
         return materializedFiles;
     })));
+}
 
+/**
+ * @deprecated
+ * Materializes a versioned schema file in the directory.
+ *
+ * @param {string} schemaDirectory directory in which to materialize schema
+ * @param {Object} schema Schema to materialize
+ * @param {Object} options
+ * @return {Promise<string>} path of newly materialized files
+ */
+async function materializeSchemaVersion(schemaDirectory, schema, options = {}) {
+    return materializeSchemaToPath(schemaDirectory, schema, options)
 }
 
 /**
@@ -676,7 +712,7 @@ async function materializeModifiedSchemas(options = {}) {
                 const schemaDirectory = path.dirname(schemaPath);
                 options.log.info(`Materializing ${schemaPath}...`);
                 const schema = await readObject(schemaPath);
-                return materializeSchemaVersion(
+                return materializeSchemaToPath(
                     schemaDirectory,
                     schema,
                     options
@@ -859,8 +895,28 @@ async function materializeAllSchemas(options = {}) {
 
     return _.flatten(await Promise.all(_.map(
         currentSchemasInfo,
-        info => materializeSchemaVersion(path.dirname(info.path), info.schema, options)
+        info => materializeSchemaToPath(path.dirname(info.path), info.schema, options)
     )));
+}
+
+/**
+ * Traverses through all the fields in the schema, including nested properties, and
+ * sets the numeric bounds specified in options when the object type is number.
+ * @param {Object} schema
+ * @param {Array} bounds as specified in options
+ * @return {Object} schema with bounded numeric fields
+ */
+function enforceNumericBounds(schema, bounds) {
+    const schemaCopy = _.cloneDeep(schema);
+    const enforcedMin = bounds[0];
+    const enforcedMax = bounds[1];
+    traverseSchema(schemaCopy, (obj) => {
+        if (['number', 'integer', 'float'].includes(obj.type)) {
+            obj.minimum = Math.max(enforcedMin, obj.minimum || -Infinity);
+            obj.maximum = Math.min(enforcedMax, obj.maximum || Infinity);
+        }
+    })
+    return schemaCopy;
 }
 
 
@@ -872,7 +928,8 @@ module.exports = {
     gitAdd,
     installGitHook,
     getSchemaById,
-    dereferenceSchema,
+    materializeSchema,
+    materializeSchemaToPath,
     materializeSchemaVersion,
     materializeModifiedSchemas,
     materializeAllSchemas,

lib/tests/structure.js
@@ -105,8 +105,7 @@ function declareTests(options = { logLevel: 'warn' }) {
                         currentSchemaInfo.version,
                         `Current and latest schema versions read from ${options.schemaVersionField} do not match`
                     );
-
-                    const dereferencedCurrentSchema = await jsonschemaTools.dereferenceSchema(
+                    const dereferencedCurrentSchema = await jsonschemaTools.materializeSchema(
                         currentSchemaInfo.schema,
                         options,
                     );

test/test.js
@@ -8,7 +8,8 @@ const yaml = require('js-yaml');
 const assert = require('assert');
 
 const {
-    materializeSchemaVersion,
+    materializeSchemaToPath,
+    materializeSchema,
     findSchemasByTitleAndMajor,
     readConfig,
     getSchemaById,
@@ -89,9 +90,30 @@ const expectedBasicDereferencedSchema = {
     },
     required: ['$schema', 'test'],
 };
+
+const expectedBasicDereferencedSchemaBounded = {
+    title: 'basic',
+    description: 'Schema used for simple tests',
+    $id: '/basic/1.2.0',
+    $schema: 'https://json-schema.org/draft-07/schema#',
+    type: 'object',
+    additionalProperties: false,
+    properties: {
+        $schema: {
+            type: 'string',
+            description: 'The URI identifying the jsonschema for this event. This may be just a short uri containing only the name and revision at the end of the URI path.  e.g. /schema_name/12345 is acceptable. This often will (and should) match the schema\'s $id field.\n',
+        },
+        test_number: {
+            type: 'number',
+            minimum: -400,
+            maximum: 1200
+        }
+    },
+    required: ['$schema', 'test'],
+};
 /* eslint camelcase: 0 */
 
-describe('materializeSchemaVersion', function() {
+describe('materializeSchemaToPath', function() {
     let tests = [
         {
             name: 'should materialize new yaml version from file with extensionless symlink',
@@ -248,7 +270,7 @@ describe('materializeSchemaVersion', function() {
             const schemaDirectory = path.dirname(schemaFile);
             const schema = yaml.safeLoad(await fse.readFile(schemaFile, 'utf-8'));
 
-            const materializedFiles = await materializeSchemaVersion(
+            const materializedFiles = await materializeSchemaToPath(
                 schemaDirectory, schema, test.options
             );
 
@@ -392,7 +414,35 @@ describe('getSchemaById', function() {
     });
 });
 
+describe('Numeric bounds enforcement', function() {
+    it('should apply maximum and minimum by default if bounds aren\'t configured', async () => {
+        const customOptions = {};
+        const options = readConfig(customOptions, true);
+        const materializedSchema = await materializeSchema(expectedBasicDereferencedSchema, options);
+        assert(materializedSchema.properties.test_integer.minimum === Number.MIN_SAFE_INTEGER);
+        assert(materializedSchema.properties.test_integer.maximum === Number.MAX_SAFE_INTEGER);
+    });
 
+    it('should overwrite maximum and minimum if configured are out of bounds', async () => {
+        const customOptions = {
+            enforcedNumericBounds: [1,2]
+        };
+        const options = readConfig(customOptions, true);
+        const materializedSchema = await materializeSchema(expectedBasicDereferencedSchemaBounded, options);
+        assert(materializedSchema.properties.test_number.minimum === 1);
+        assert(materializedSchema.properties.test_number.maximum === 2);
+    });
+
+    it('should not apply bounds if option is null', async () => {
+        const customOptions = {
+            enforcedNumericBounds: null
+        };
+        const options = readConfig(customOptions, true);
+        const materializedSchema = await materializeSchema(expectedBasicDereferencedSchema, options);
+        assert(!materializedSchema.properties.test_integer.minimum);
+        assert(!materializedSchema.properties.test_integer.maximum);
+    })
+})
 
 describe('Test Schema Repository Tests', function() {
     let fixture;

commit 46cdbeb904a68da74ce01f6a5582d14c3d418180

Fix script dereference bug introduced in 706c5cd9
jsonschema-tools dereference was calling materializeSchema instead of dereference schema.

scripts/jsonschema-tools.js
@@ -6,7 +6,7 @@ const path  = require('path');
 const yargs = require('yargs');
 
 const {
-    materializeSchema,
+    dereferenceSchema,
     materializeSchemaToPath,
     materializeModifiedSchemas,
     materializeAllSchemas,
@@ -181,7 +181,7 @@ async function dereference(args) {
 
     const schemas = await Promise.all(schemaPaths.map(async (schemaPath) => {
         try {
-            return await materializeSchema(await readObject(schemaPath), options);
+            return await dereferenceSchema(await readObject(schemaPath), options);
         } catch (err) {
             options.log.fatal(err, `Failed dereferencing schema at ${schemaPath}`);
             process.exit(1);